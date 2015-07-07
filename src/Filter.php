85bd2a2361896d96d5cbc5de3d519b614484a585
Paul M. Jones <pmjones88@gmail.com>
2015-07-07 09:20:06 -0500

    add a class of constants

---
+++ src/Filter.php
@@ -0,0 +1,44 @@
+<?php
+namespace Aura\Filter;
+
+class Filter
+{
+    const ALNUM = 'alnum';
+    const ALPHA = 'alpha';
+    const BETWEEN = 'between';
+    const BLANK = 'blank';
+    const BOOL = 'bool';
+    const CALLBACK = 'callback';
+    const CREDIT_CARD = 'creditCard';
+    const DATE_TIME = 'dateTime';
+    const EMAIL = 'email';
+    const EQUAL_TO_FIELD = 'equalToField';
+    const EQUAL_TO_VALUE = 'equalToValue';
+    const FIELD = 'field';
+    const FLOAT = 'float';
+    const IN_KEYS = 'inKeys';
+    const INT = 'int';
+    const IN_VALUES = 'inValues';
+    const IP = 'ip';
+    const ISBN = 'isbn';
+    const LOCALE = 'locale';
+    const MAX = 'max';
+    const MIN = 'min';
+    const NOW = 'now';
+    const REGEX = 'regex';
+    const REMOVE = 'remove';
+    const STRICT_EQUAL_TO_FIELD = 'strictEqualToField';
+    const STRICT_EQUAL_TO_VALUE = 'strictEqualToValue';
+    const STRING = 'string';
+    const STRLEN = 'strlen';
+    const STRLEN_BETWEEN = 'strlenBetween';
+    const STRLEN_MAX = 'strlenMax';
+    const STRLEN_MIN = 'strlenMin';
+    const TRIM = 'trim';
+    const UPLOAD = 'upload';
+    const URL = 'url';
+    const UUID = 'uuid';
+    const UUID_HEXONLY = 'uuidHexonly';
+    const VALUE = 'value';
+    const WORD = 'word';
+}
